7be07c41a2492eb15eacde1c3f1ffb993237ba7d
Vishaal Meduri <71046664+vmeduri1@users.noreply.github.com>
2021-01-25 01:22:21 -0600

    Add files via upload

---
+++ practice-week-2/problems/02-spiderManWriters.js
@@ -0,0 +1,43 @@
+/***********************************************************************
+Welcome to The Article Company! At AC, you have been tasked with a
+very important job! Your boss says we need more articles about Spiderman!
+But, uh-oh! Some of the writers did not write articles about Spiderman.
+You have been given a long string that contains a list of every writer,
+and a value that shows if they have written an article about Spiderman.
+You must write a function, 'spiderManWriters(string)' and convert its contents
+to an object that "ONLY" contains key value pairings of writers who did "NOT"
+write about Spiderman.
+
+Assumptions:
+You may assume that the order of the key value pairs in the object does NOT matter.
+
+Examples:
+
+str1 = 'Angela T: wrote about Spiderman, Miah B: no Spiderman, Adrian R: wrote about Spiderman'
+str2 = 'Tom B: no Spiderman, Justin N: no Spiderman, Emily B: wrote about Spiderman'
+***********************************************************************/
+
+    // Your code here
+    function spiderManWriters(string) {
+        const obj = {};
+        const array = string.split(', ');
+        for (let i = 0; i < array.length; i++) {
+            const current = array[i];
+            if (current.includes("no Spiderman")) {
+                let name = current.split(':')[0];
+                obj[name] = 'no Spiderman';
+            }
+        }
+        return obj;
+    };
+
+str1 = 'Angela T: wrote about Spiderman, Miah B: no Spiderman, Adrian R: wrote about Spiderman'
+str2 = 'Tom B: no Spiderman, Justin N: no Spiderman, Emily B: wrote about Spiderman'
+console.log(spiderManWriters(str1));
+console.log(spiderManWriters(str2));
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
+try{
+    module.exports = spiderManWriters;
+} catch(e){
+    module.exports = null;
+}
